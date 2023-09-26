30ddec1e77ac6fe8ba4cd5d36ec9dfd0973ae74c
Nashjyoti <jyoti.chauhan@nashtechglobal.com>
2023-09-26 12:46:18 +0530

    sixth commit

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -56,7 +56,7 @@ module.exports = {
       
       webdriver: {
         start_process: true,
-        server_path: '/home/knoldus/Jyoti/Chrome driver/chrome-linux64'
+        server_path: '/home/knoldus/Jyoti/Chrome driver/chromedriver-linux64/chromedriver'
       },
       
     },
